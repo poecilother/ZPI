542894576fb236edfcebf0fc8737b2950424f3aa
Suchy <senity@outlook.com>
2020-03-18 17:32:19 +0100

    Init

---
+++ client/src/main.js
@@ -0,0 +1,20 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ client/src/views/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="auth">
+  </div>
+</template>
+
+<script>
+import Alert from '@/components/Alert.vue'
+
+export default {
+  name: 'Auth',
+  components: {
+    Alert
+  }
+}
+</script>
