559c8072df56f98fdb037f15423dc714739501ea
unknown <wieviur@gmail.com>
2020-03-18 12:23:44 +0100

    Added initial files

---
+++ Projekt/Diagramy i modele/.delete_bak.py
@@ -0,0 +1,10 @@
+import os, re
+
+
+def purge(dir, pattern):
+    for f in os.listdir(dir):
+        if re.search(pattern, f):
+            os.remove(os.path.join(dir, f))
+
+
+purge('.', '^.*\.vpp\.bak.*$')
