4343b11c28609ecea817f297e4572d8582cd3871
unknown <wieviur@gmail.com>
2020-04-25 15:02:43 +0200

    A lot of changes

---
+++ server/src/controllers/getBoxData.js
@@ -0,0 +1,9 @@
+const Imap = require('imap');
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
+
+function getBoxData (req, res, next) {
+    
+}
+
+module.exports = getBoxData;
--- server/src/routes/mail.js
+++ server/src/routes/mail.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const delBox = require('../controllers/delBox');
 
 router.use('/imap', require('../routes/imap'));
-router.use('./pop3', require('../routes/pop3'));
+router.use('/pop3', require('../routes/pop3'));
 
 router.delete('/del', delBox, () => {
     res.json({ success: res.locals.success, msg: res.locals.msg })
