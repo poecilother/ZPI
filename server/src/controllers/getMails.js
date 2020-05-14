df8e55a72cacb60c1fd3812a5c13f2d7a356dfab
unknown <wieviur@gmail.com>
2020-05-14 12:28:23 +0200

    A lot of changes

--- server/src/controllers/getMails.js
+++ server/src/controllers/getMails.js
@@ -2,82 +2,111 @@ const User = require('../models/users');
 const jwt = require('jsonwebtoken');
 
 async function getMails (req, res, next) {
+
+    try {
+        if (!req.query.folder) {
+            return res.json({
+                success: 0,
+                msg: 'Brak numeru folderu'
+            }); 
+        }
     
-    if (!req.query.folder) {
-        return res.json({
-            success: 0,
-            msg: 'Brak numeru folderu'
-        }); 
-    }
+        if (!req.query.user) {
+            jwt.verify(req.header('authorization'), process.env.JWT_SECRET, async (err, decodedToken) => {
+                if (err) {
+                    return res.json({ success: -1 });
+                } else {
+                    req.userId = decodedToken.sub;
+    
+                    const foundUser = await User.findOne({
+                        '_id':req.userId
+                    }, 'mailBoxes');
+    
+                    if (foundUser) {
+                        let promises = [];
+    
+                        if (foundUser.mailBoxes.length == 0) {
+                            return res.json({
+                                success: 0,
+                                msg: 'Nie masz żadnych wiadomości'
+                            });  
+                        }
+                        
+                        mails = [];
 
-    if (!req.query.user) {
-        jwt.verify(req.header('authorization'), process.env.JWT_SECRET, async (err, decodedToken) => {
-            if (err) {
-                return res.json({ success: -1 });
-            } else {
-                req.userId = decodedToken.sub;
-
-                const foundUser = await User.findOne({
-                    '_id':req.userId
-                }, 'mailBoxes');
-
-                if (foundUser) {
-                    let promises = [];
-                    for (let i = 0; i < foundUser.mailBoxes.length; i++) {
-                        for (j = 0; j < foundUser.mailBoxes[i].mails.length; j++) {
-                            if (foundUser.mailBoxes[i].mails[j].folder == req.query.folder) {
-                                promises.push(pushMails(foundUser, i, j));
+                        console.log('MAILBOXES: ', foundUser.mailBoxes.length)
+                        for (let i = 0; i < foundUser.mailBoxes.length; i++) {
+                            if (!foundUser.mailBoxes[i].mails) {
+                                i++;
+                            } else {
+                                for (let j = 0; j < foundUser.mailBoxes[i].mails.length; j++) {
+                                    if (foundUser.mailBoxes[i].mails[j].folder == req.query.folder) {
+                                        promises.push(pushMails(foundUser, i, j, mails));
+                                    }
+                                }
+                            }
+                        }
+                        
+                        Promise.all(promises).then((mails) => {
+                            console.log('Długość mails: ', mails[0].length)
+                            sendMails = [];
+                            for (let i = 0; i < mails[0].length; i++) {
+                                sendMails.push(mails[0][i]);
+                            }
+                            return res.json({
+                                success: 1,
+                                mails: sendMails
+                            });
+                        }).catch(function (err) {
+                            console.log('PROMISE: ', err);
+                       });
+                    }
+                }
+            });
+        } else {
+            jwt.verify(req.header('authorization'), process.env.JWT_SECRET, async (err, decodedToken) => {
+                if (err) {
+                    return res.json({ success: -1 });
+                } else {
+                    req.userId = decodedToken.sub;
+        
+                    mails = new Array();
+        
+        
+                    const foundUser = await User.findOne({
+                        '_id':req.userId,
+                        'mailBoxes.user': req.query.user
+                    }, 'mailBoxes.$');
+                    
+                    if (foundUser) {
+                        for (j = 0; j < foundUser.mailBoxes[0].mails.length; j++) {
+                            if (foundUser.mailBoxes[0].mails[j].folder == req.query.folder) {
+                                mails.push(foundUser.mailBoxes[0].mails[j])
                             }
                         }
                     }
                     
-                    Promise.all(promises).then((mails) => {
-                        return res.json({
-                            success: 1,
-                            mails: mails
-                        });
-                    });
                 }
-            }
-        });
-    }
-
-    jwt.verify(req.header('authorization'), process.env.JWT_SECRET, async (err, decodedToken) => {
-        if (err) {
-            return res.json({ success: -1 });
-        } else {
-            req.userId = decodedToken.sub;
-
-            mails = new Array();
-
-
-            const foundUser = await User.findOne({
-                '_id':req.userId,
-                'mailBoxes.user': req.query.user
-            }, 'mailBoxes.$');
-            
-            if (foundUser) {
-                for (j = 0; j < foundUser.mailBoxes[0].mails.length; j++) {
-                    if (foundUser.mailBoxes[0].mails[j].folder == req.query.folder) {
-                        mails.push(foundUser.mailBoxes[0].mails[j])
-                    }
-                }
-            }
-            
+                return res.json({
+                    success: 1,
+                    mails: mails
+                });
+            });
         }
-        return res.json({
-            success: 1,
-            mails: mails
-        });
-    });
+    } catch (err) {
+        console.log("ERROR: ", err)
+    }
 }
 
-async function pushMails(foundUser, i, j) {
-    return new Promise((resolve) => {
-        mails.push(foundUser.mailBoxes[i].mails[j])
-        console.log(foundUser.mailBoxes[i].user)
-        return resolve(mails)
-    });
+async function pushMails(foundUser, i, j, mails) {
+    try{
+        return new Promise((resolve) => {
+            mails.push(foundUser.mailBoxes[i].mails[j])
+            return resolve(mails)
+        });
+    } catch (err) {
+        console.log('ERROR: ', err);
+    }
 }
 
 module.exports = getMails;
